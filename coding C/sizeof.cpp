ba2e2fb8193cf7691b66eef6475d10c45337dabd
Ankitsm38 <122076192+Ankitsm38@users.noreply.github.com>
2023-01-07 08:53:59 +0530

    size of

---
+++ coding C/sizeof.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main() {
+    int a;
+    float f;
+    double d;
+    char c;
+    printf("The size of int : %d\n",sizeof(a));
+    printf("the size of float: %d\n",sizeof(f));
+    printf("the size of double: %d\n",sizeof(d));
+    printf("the size of char: %d",sizeof(c));
+    return 0;
+}
